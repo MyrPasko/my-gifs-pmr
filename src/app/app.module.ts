b30f44dc73f4e61268cdab82f7d6d960d9ff433e
MyrPasko <myroslavpasko@gmail.com>
2019-02-21 10:27:52 +0200

    Second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AuthModule} from './auth/auth.module';
+import {AppRoutingModule} from './app-routing.module';
+import {UsersService} from './shared/services/users.service';
+import {AuthService} from './shared/services/auth.service';
+import { MainComponent } from './system/main/main.component';
+
+// import {SystemModule} from './system/system.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AuthModule,
+    AppRoutingModule,
+    // SystemModule
   ],
-  providers: [],
+  providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,25 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {User} from '../models/user.model';
+import {BaseApi} from '../core/base-api';
+
+
+@Injectable()
+
+export class UsersService extends BaseApi {
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  getUserByEmail(email: string) {
+    return this.get(`users?email=${email}`);
+      // .map((user: User[]) => {
+      //   return user[0] ? user[0] : undefined;
+      // });
+  }
+
+  createNewUser(user: User) {
+    return this.post('users', user);
+  }
+
+}
